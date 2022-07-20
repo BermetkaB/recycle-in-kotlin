a1a49c3d675ba92adc1fe5f184c23951d3705c1c
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-21 01:06:49 +0600

    recycle

--- app/src/main/java/com/example/recycle_in_kotlin/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/recycle_in_kotlin/adapter/UserAdapter.kt
@@ -3,30 +3,34 @@ package com.example.recycle_in_kotlin.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recycle_in_kotlin.R
 import com.example.recycle_in_kotlin.model.UserModel
 
-class UserAdapter:RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
-
+class UserAdapter :RecyclerView.Adapter<UserAdapter.UserViewHolder>(){
     private var userList= emptyList<UserModel>()
-    class UserViewHolder(view: View):RecyclerView.ViewHolder(view)
+    class UserViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-       val view=LayoutInflater.from(parent.context).inflate(R.layout.user_item_layout,parent,false)
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.user_item_layout,parent,false)
         return UserViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.itemView.tv
 
     }
 
     override fun getItemCount(): Int {
-        return userList.size
-    }
+     return userList.size   }
+
 
 }
 
+/*
+val view=LayoutInflater.from(parent.context).inflate(R.layout.user_item_layout,parent,false)
+return UserViewHolder(view)
 
-
-
+private var userList= emptyList<UserModel>()
+   */
